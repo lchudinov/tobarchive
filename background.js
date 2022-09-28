6093dba7ef97a1a12cdee1870f8c1283c48c42ae
Leonty Chudinov <leonty@inbox.ru>
2022-09-28 20:12:39 +0500

    Migrate to manifest v3
    Signed-off-by: Leonty Chudinov <leonty@inbox.ru>

--- background.js
+++ background.js
@@ -1,17 +1,38 @@
 // background.js
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  console.log(`background script received request`, request);
-  const { url } = request;
-  chrome.downloads.download({ url });
-});
+chrome.runtime.onMessage.addListener(
+  (request, sender, sendResponse) => {
+    console.log(`response received request`, request);
+    if (typeof request === 'object' && typeof request.url === 'string' && typeof request.filename === 'string') {
+      const { url, filename } = request;
+      console.log(`url ${url} filename ${filename}`);
+      const saveAs = false;
+      chrome.downloads.download({ url,  filename, saveAs }, (arg) => console.log(`downloading`, arg, chrome.runtime.lastError));
+    } else {
+      console.log(`bad request`);
+    }
+  }
+);
 
-chrome.browserAction.onClicked.addListener(
+const findImage = function () {
+  const selector = '#MainPlaceHolder__storageViewerControl_DeepZoomImageViewer_seadragonContainer div div div img';
+  const inputSelector = '#MainPlaceHolder__storageViewerControl_FilesDropDownList_I';
+  const image = document.querySelector(selector);
+  const input = document.querySelector(inputSelector);
+  if (image && input) {
+    console.log(`image found src = ${image.src}, file ${input.value}`);
+    chrome.runtime.sendMessage({ url: image.src, filename: input.value.toLowerCase().replaceAll('\\', '_') })
+  } else {
+    console.log(`image not found`);
+  }
+}
+
+chrome.action.onClicked.addListener(
   (tab) => {
-    chrome.tabs.sendMessage(tab.id, { action: 'download' }, (response) => {
-      console.log(`response received request`, response);
-      const { url, filename } = response;
-      chrome.downloads.download({ url, filename });
+    console.log(`extension icon clicked`);
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      func: findImage
     });
   }
 );
